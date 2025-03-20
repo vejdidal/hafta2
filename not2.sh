7f82bb1b932d4ce3b7348f990b5b22787888a92e
vejdi dal <vejdidal@gmail.com>
2025-03-20 16:38:00 +0300

    first commit

---
+++ not2.sh
@@ -0,0 +1,17 @@
+#1/bin/bash
+echo "isminizi girin"
+read ad
+echo "isminiz: " $ad
+echo "soyisim girin"
+read soyad
+echo "soyad:" $soyad
+echo "vize girin"
+read vize
+v=$((vize/2))
+echo "final girin"
+read final
+f=$((final/2))
+echo " ortalamanız:" $v+$f
+
+
+# hafta2
